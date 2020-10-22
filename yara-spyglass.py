0d62e662b9db18d5a3745ecd8ec3cb554fcfdbe8
August Johnson <august.johnson@gmail.com>
2020-10-21 21:19:46 -0500

    Basic POC.

--- yara-spyglass.py
+++ yara-spyglass.py
@@ -2,10 +2,20 @@ import yara
 import requests
 
 
-rules = yara.compile(source='rule dummy {condition:true}')
+rules = yara.compile(filepath='./rules/nginx.yara')
 
-x = requests.get('https://augustjohnson.net')
+with open('testsites.txt','r') as testTargetFile:
+    targets = testTargetFile.readlines()
 
-matches = rules.match(data=str(x.headers))
+    for target in targets:
+        try:
+            response = requests.get(target.rstrip())
+        except:
+            print("could not connect to {}".format(target))
+            continue
+        matches = rules.match(data=str(response.headers))
+        if 'main' not in matches: print("{} had no matches".format(target.rstrip()))
+        else:
+            for match in matches['main']:
+                if match['matches']: print('{} matched on {}'.format(target,match['rule']))
 
-print(matches)
